41f74f2a2292a36b2e34f6f223ce2a2113a00f01
Filan <fedosenko009@mail.ru>
2024-08-28 00:07:44 +0300

    Auth

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,19 +1,21 @@
 <template>
-    <section class="section menu__section">
-        <div class="container menu__container grid-row jc-sb">
-            <router-link to="/" class="link menu__logo">
-                <img src="@/assets/img/logo/logo.png" alt="Деловая Клякса" class="menu__logo_img">
-            </router-link>
-            <nav class="grid menu__nav grid-row jc-sb gap-10 ai-c">
-                <router-link to="/" class="link">Главная</router-link>
-                <router-link to="/order" class="link">Заказ</router-link>
-            </nav>
-            <nav class="grid menu__nav grid-row jc-sb gap-10 ai-c">
-                <router-link to="/register" class="btn-white">Регистрация</router-link>
-                <router-link to="/login" class="btn-main">Вход</router-link>
-            </nav>
-        </div>
-    </section>
+    <header>
+        <section class="section menu__section">
+            <div class="container menu__container grid-row jc-sb">
+                <router-link to="/" class="link menu__logo">
+                    <img src="@/assets/img/logo/logo.png" alt="Деловая Клякса" class="menu__logo_img">
+                </router-link>
+                <nav class="grid menu__nav grid-row jc-sb gap-10 ai-c">
+                    <router-link to="/" class="link">Главная</router-link>
+                    <router-link to="/order" class="link">Заказ</router-link>
+                </nav>
+                <nav class="grid menu__nav grid-row jc-sb gap-10 ai-c">
+                    <router-link to="/register" class="btn-white">Регистрация</router-link>
+                    <router-link to="/login" class="btn-main">Вход</router-link>
+                </nav>
+            </div>
+        </section>
+    </header>
 </template>
 
 <script setup>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,19 +1,20 @@
 <template>
     <section class="section auth__section">
-        <div class="container auth__section gap-50">
-            <h2 class="h2 auth__title w-700 ta-c">Вход</h2>
+        <div class="container auth__container gap-50 ac-c">
             <form action="#" class="form auth__form" enctype="application/x-www-form-urlencoded">
+                <h2 class="h2 auth__title w-700 ta-c mb-25 js-s">Вход</h2>
                 <label for="email" class="label">
                     <input type="email" class="input auth__input" id="email" placeholder="">
-                    <span>Ваша почта*</span>
+                    <span>Ваша email*</span>
                 </label>
-                <label for="password" class="label">
+                <label for="password" class="label mb-25">
                     <input type="password" class="input auth__input" id="password" placeholder="">
                     <span>Ваш пароль*</span>
                 </label>
+                <button type="submit" class="btn-main js-s">Регистрация</button>
             </form>
-            <button type="submit" class="btn-main">Войти</button>
         </div>
+        <div class="container auth__container auth__container_img"></div>
     </section>
 </template>
 
@@ -22,5 +23,21 @@
 </script>
 
 <style scoped>
-
+    .auth__section{
+        grid-auto-flow: unset;
+        grid-template-columns: 2fr 3fr;
+        min-height: 100vh;
+        align-items: unset;
+        align-content: unset;
+        padding-bottom: 0;
+    }
+    .auth__container_img{
+        background-image: url("@/assets/img/auth/login.jpg");
+        background-position: center center;
+        background-size: contain;
+        background-repeat: no-repeat;
+        position: relative;
+        height: 100%;
+        background-color: var(--colorMain);
+    }
 </style>
