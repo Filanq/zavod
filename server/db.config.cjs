a297795bcecf3924b194de7f660efa41b00c8ec1
Filan <fedosenko009@mail.ru>
2024-08-30 13:14:41 +0300

    Done

--- server/db.config.cjs
+++ server/db.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "zavod"
+    host: "147.45.160.171",
+    user: "gen_user",
+    password: "T_~0/AJx%okV0L",
+    database: "zavod",
+    port: "3306"
 };
